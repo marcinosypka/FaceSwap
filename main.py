4b58561fe7b1347fdb7f9c65dfb91b4384ac8e7d
Marcin Osypka <marcin.osypka@gmail.com>
2019-01-29 12:58:39 +0100

    add video, use ip webcam to obtain destination images

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import os
 import cv2
 import dlib
 import argparse
+from time import time
 
 import numpy as np
 
 from face_detection import face_detection
 from face_points_detection import face_points_detection
 from face_swap import warp_image_2d, warp_image_3d, mask_from_points, apply_mask, correct_colours, transformation_from_points
+
 
 def select_face(im, r=10):
     faces = face_detection(im)
@@ -57,73 +59,88 @@ def select_face(im, r=10):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='FaceSwapApp')
     parser.add_argument('--src', required=True, help='Path for source image')
-    parser.add_argument('--dst', required=True, help='Path for target image')
-    parser.add_argument('--out', required=True, help='Path for storing output images')
+    #parser.add_argument('--dst', required=True, help='Path for target image')
+    #parser.add_argument('--out', required=True, help='Path for storing output images')
     parser.add_argument('--warp_2d', default=False, action='store_true', help='2d or 3d warp')
     parser.add_argument('--correct_color', default=False, action='store_true', help='Correct color')
     parser.add_argument('--no_debug_window', default=False, action='store_true', help='Don\'t show debug window')
     args = parser.parse_args()
 
+    #Initialize camera
+
+    cap = cv2.VideoCapture("http://192.168.0.87:8080/video")
+    if not cap.isOpened():
+        raise ("Camera cannot be opened")
+    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
     # Read images
     src_img = cv2.imread(args.src)
-    dst_img = cv2.imread(args.dst)
-
+    cv2.imshow("Source image", src_img)
     # Select src face
     src_points, src_shape, src_face = select_face(src_img)
-    # Select dst face
-    dst_points, dst_shape, dst_face = select_face(dst_img)
 
-    w, h = dst_face.shape[:2]
-    
-    ### Warp Image
-    if not args.warp_2d:
-        ## 3d warp
-        warped_src_face = warp_image_3d(src_face, src_points[:48], dst_points[:48], (w, h))
-    else:
-        ## 2d warp
+    if args.warp_2d:
         src_mask = mask_from_points(src_face.shape[:2], src_points)
         src_face = apply_mask(src_face, src_mask)
-        # Correct Color for 2d warp
-        if args.correct_color:
-            warped_dst_img = warp_image_3d(dst_face, dst_points[:48], src_points[:48], src_face.shape[:2])
-            src_face = correct_colours(warped_dst_img, src_face, src_points)
-        # Warp
-        warped_src_face = warp_image_2d(src_face, transformation_from_points(dst_points, src_points), (w, h, 3))
+    while True:
 
-    ## Mask for blending
-    mask = mask_from_points((w, h), dst_points)
-    mask_src = np.mean(warped_src_face, axis=2) > 0
-    mask = np.asarray(mask*mask_src, dtype=np.uint8)
+        ret, dst_img = cap.read()
+        #clear buffer
+        for i in range(20):
+            ret, dst_img = cap.read()
+        # Select dst face
+        dst_points, dst_shape, dst_face = select_face(dst_img)
 
-    ## Correct color
-    if not args.warp_2d and args.correct_color:
-        warped_src_face = apply_mask(warped_src_face, mask)
-        dst_face_masked = apply_mask(dst_face, mask)
-        warped_src_face = correct_colours(dst_face_masked, warped_src_face, dst_points)
-    
-    ## Shrink the mask
-    kernel = np.ones((10, 10), np.uint8)
-    mask = cv2.erode(mask, kernel, iterations=1)
-    ##Poisson Blending
-    r = cv2.boundingRect(mask)
-    center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
-    output = cv2.seamlessClone(warped_src_face, dst_face, mask, center, cv2.NORMAL_CLONE)
+        w, h = dst_face.shape[:2]
 
-    x, y, w, h = dst_shape
-    dst_img_cp = dst_img.copy()
-    dst_img_cp[y:y+h, x:x+w] = output
-    output = dst_img_cp
+        ### Warp Image
+        if not args.warp_2d:
+            ## 3d warp
+            warped_src_face = warp_image_3d(src_face, src_points[:48], dst_points[:48], (w, h))
+        else:
+            ## 2d warp
+            # Correct Color for 2d warp
+            if args.correct_color:
+                warped_dst_img = warp_image_3d(dst_face, dst_points[:48], src_points[:48], src_face.shape[:2])
+                src_face = correct_colours(warped_dst_img, src_face, src_points)
+            # Warp
+            warped_src_face = warp_image_2d(src_face, transformation_from_points(dst_points, src_points), (w, h, 3))
 
-    dir_path = os.path.dirname(args.out)
-    if not os.path.isdir(dir_path):
-        os.makedirs(dir_path)
+        ## Mask for blending
+        mask = mask_from_points((w, h), dst_points)
+        mask_src = np.mean(warped_src_face, axis=2) > 0
+        mask = np.asarray(mask*mask_src, dtype=np.uint8)
 
-    cv2.imwrite(args.out, output)
+        ## Correct color
+        if not args.warp_2d and args.correct_color:
+            warped_src_face = apply_mask(warped_src_face, mask)
+            dst_face_masked = apply_mask(dst_face, mask)
+            warped_src_face = correct_colours(dst_face_masked, warped_src_face, dst_points)
 
-    ##For debug
-    if not args.no_debug_window:
-        cv2.imshow("From", dst_img)
-        cv2.imshow("To", output)
-        cv2.waitKey(0)
-        
-        cv2.destroyAllWindows()
+        ## Shrink the mask
+        kernel = np.ones((10, 10), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        ##Poisson Blending
+        r = cv2.boundingRect(mask)
+        center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
+        output = cv2.seamlessClone(warped_src_face, dst_face, mask, center, cv2.NORMAL_CLONE)
+
+        x, y, w, h = dst_shape
+        dst_img_cp = dst_img.copy()
+        dst_img_cp[y:y+h, x:x+w] = output
+        output = dst_img_cp
+
+        # dir_path = os.path.dirname(args.out)
+        # if not os.path.isdir(dir_path):
+        #     os.makedirs(dir_path)
+
+        #cv2.imwrite(args.out, output)
+
+        ##For debug
+        if not args.no_debug_window:
+            cv2.imshow("Orginal", dst_img)
+            cv2.imshow("FaceSwapped", output)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
